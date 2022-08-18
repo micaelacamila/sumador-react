254c3b4932b2286c6f2f8ab5d75ea11f84ac2a4b
Micaela Patiño <micaela.patino@ucb.edu.bo>
2022-08-17 23:10:21 -0400

    Read

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-function App() {
-  return <div className="App">Sumador</div>;
-}
+import React from 'react';
 
-export default App;
+import axios from 'axios';
+
+export default class App extends React.Component {
+  state={
+    children:[]
+  }
+  componentDidMount(){
+    const axios = require('axios');
+    axios.get("http://localhost:36882/api/Children").then(resp=>{
+      //console.log(resp.data);
+      const children = resp.data;
+      this.setState({children});
+      console.log(this.state.children);
+    });
+  }
+  render(){
+    return (
+      <div>
+        <h1>List of Children</h1>
+        <ul>
+          {this.state.children.map(child=><li>{child.name} | {child.gender} | {child.birthDate}</li>)}
+        </ul>
+      </div>
+      
+     );
+  }
+}
